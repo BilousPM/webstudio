b5c54b2ea8caefb24c104c36eb2d032a84f4e8d0
Petro Bilous <bilouspm@gmail.com>
2022-12-15 22:14:55 +0200

    add js

---
+++ src/js/anchor-scroll.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+
+$(document).ready(function () {
+// Add smooth scrolling to all links
+    $('a').on('click', function (event) {
+        // make sure this.hash has a value before overriding default behavior
+        if (this.hash !== '') {
+            
+
+        }
+
+
+
+
+    })
+
+// ------------------ + - 57 хвилина відоса ---------
+    
+
+
+
+
+
+
+
+
+})
